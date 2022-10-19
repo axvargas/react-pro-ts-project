2c02c2b177a7d43c4f799bb3629565612fde5ba1
Andres X. Vargas <axvargas@fiec.espol.edu.ec>
2022-10-18 19:55:36 -0500

    Control props alternative

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { OnChangeArgs, Product } from '../interfaces/interfaces';
 
 interface useProductArgs {
@@ -11,15 +11,11 @@ interface useProductArgs {
 export const useProduct = ({onChange, product, value = 0} : useProductArgs) => {
   const [counter, setCounter] = useState(value)
 
-  const isControlled = useRef(!!onChange)
   useEffect(() => {
     setCounter(value)
   }, [value])
   
   const increaseBy = (value: number) => {
-    if (isControlled.current) {
-      return onChange && onChange({product, quantity:value})
-    }
     const newCounter = Math.max(0, counter + value);
     setCounter(newCounter)
     onChange && onChange({product, quantity: newCounter})
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -6,24 +6,24 @@ export const useShoppingCart = () => {
   
   const onProductQuantityChange = ({product, quantity}: {product: Product, quantity: number}) => {  
     setShoppingCart(oldShoppingCart => {
-      const shoppingCartProduct: ShoppingCartProduct = oldShoppingCart[product.id] || {...product, quantity: 0}
-      if(Math.max(0, shoppingCartProduct.quantity + quantity) > 0) {
-        shoppingCartProduct.quantity += quantity;
-        return {...oldShoppingCart, [product.id]: shoppingCartProduct}
-      } 
-      const newShoppingCart = {...oldShoppingCart}
-      delete newShoppingCart[product.id]
-      return newShoppingCart
-      // const newShoppingCart = {...oldShoppingCart};
-      // if (quantity === 0) {
-      //   delete newShoppingCart[product.id];
-      // } else {
-      //   newShoppingCart[product.id] = {
-      //     ...product,
-      //     quantity
-      //   }
-      // }
-      // return newShoppingCart;
+      // const shoppingCartProduct: ShoppingCartProduct = oldShoppingCart[product.id] || {...product, quantity: 0}
+      // if(Math.max(0, shoppingCartProduct.quantity + quantity) > 0) {
+      //   shoppingCartProduct.quantity += quantity;
+      //   return {...oldShoppingCart, [product.id]: shoppingCartProduct}
+      // } 
+      // const newShoppingCart = {...oldShoppingCart}
+      // delete newShoppingCart[product.id]
+      // return newShoppingCart
+      const newShoppingCart = {...oldShoppingCart};
+      if (quantity === 0) {
+        delete newShoppingCart[product.id];
+      } else {
+        newShoppingCart[product.id] = {
+          ...product,
+          quantity
+        }
+      }
+      return newShoppingCart;
     });
   }
 
